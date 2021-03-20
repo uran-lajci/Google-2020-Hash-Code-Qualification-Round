1b55fcb7380c952b6e7cb2b3e9e62ac60cbe0a0a
Eduardo <1171179@isep.ipp.pt>
2021-03-20 21:52:34 +0000

    Main menu add Class book with gets and equals

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 
     int id;
@@ -10,4 +12,28 @@ public class Book {
         this.score = score;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", score=" + score +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return getId() == book.getId() && score == book.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), score);
+    }
 }

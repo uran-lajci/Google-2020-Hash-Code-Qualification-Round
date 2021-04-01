30a5e54a3195e6781e572e60f55a7975e42deefb
up202003219 <tomas.moreira.santos.99@gmail.com>
2021-04-01 23:01:33 +0100

    Minor UI change to GA.

--- src/Main.java
+++ src/Main.java
@@ -70,7 +70,7 @@ public class Main {
 
                     System.out.println("Finished evolution.\n");
 
-                    String result = String.format("Best score: %d points in %d seconds.", ga.getCurrentBestSolution().updateScore(), timeElapsed);
+                    String result = String.format("Best score: %d points in %d milliseconds.", ga.getCurrentBestSolution().getScore(), timeElapsed);
                     System.out.println(result);
                     break;
                 case "2":
